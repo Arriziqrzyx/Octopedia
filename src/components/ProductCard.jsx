2209ccace7779ad927e713de7eed7e67f9022239
Arriziq <114041758+Arriziqrzyx@users.noreply.github.com>
2024-10-11 15:45:11 +0700

    update handle button add cart

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { addToCart } from "../store/cartSlice"; // Import addToCart action
+import { addToCart } from "../store/cartSlice";
 
 const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleAddToCart = () => {
-    dispatch(addToCart(product)); // Dispatch action untuk menambahkan produk ke cart
+    const isAuthenticated = !!localStorage.getItem("token");
+
+    if (!isAuthenticated) {
+      navigate("/login");
+    } else {
+      dispatch(addToCart(product));
+    }
   };
 
   const existingProduct = useSelector((state) =>
